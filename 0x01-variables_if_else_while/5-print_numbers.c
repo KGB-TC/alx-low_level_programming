10c3a0dc5e8358b7ef1ff965a071a3c9ad56aa73
KGB-TC <tochukwuchrisejiofor>
2022-10-14 15:39:56 -0700

    Commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+	int main(void)
+
+	{
+		int x = 0;
+
+		for (x = 0; x < 10; x++)
+
+			printf("%d\n", x);
+
+		return (0);
+
+	}
