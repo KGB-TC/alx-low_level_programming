85f6103a05ee7eeb32e28021f1a7fce1d50f64c5
KGB-TC <tochukwuchrisejiofor>
2022-10-20 16:44:00 -0700

    Commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+
+
+/**
+ * mul - multiples two integers
+ *
+ * @a: function argument
+ *
+ * @b: function argument
+ *
+ * Return: Product value
+ */
+
+
+
+int mul(int a, int b)
+
+{
+
+		return (a * b);
+
+}
