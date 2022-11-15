9e086c39474c9a4b0c2f47490e4ff2276bb4591e
KGB-TC <tochukwuchrisejiofor>
2022-11-15 05:28:53 -0800

    Commit

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,5 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+#define ABS(X) ((X) < 0 ? -(1) * (X) : (X))
+
+#endif
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,5 @@
+#ifndef LIKEMACRO_H
+#define LIKEMACRO_H
+#define SUM(x, y) (x + y)
+
+#endif
