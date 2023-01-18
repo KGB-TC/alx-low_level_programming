95ea2a6c98b75ec661b8647f72a14f5ae097a62d
KGB-TC <tochukwuchrisejiofor>
2023-01-18 00:31:29 -0800

    Comit

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,15 +1,51 @@
-#ifndef _MAIN_H_
+#ifndef MAIN_H
 
-#define _MAIN_H_
+#define MAIN_H
 
-int add(int a, int b);
+#include <stdio.h>
 
-int sub(int a, int b);
+#include <stdlib.h>
 
-int mul(int a, int b);
+#define EXIT_SUCCESS 1
 
-int div(int a, int b);
+int _putchar(char c);
 
-int mod(int a, int b);
+int _islower(int c);
 
-#endif
+int _isalpha(int c);
+
+int _abs(int n);
+
+int _isupper(int c);
+
+int _isdigit(int c);
+
+int _strlen(char *s);
+
+void _puts(char *str);
+
+char *_strcpy(char *dest, char *src);
+
+int _atoi(char *s);
+
+char *_strcat(char *dest, char *src);
+
+char *_strncat(char *dest, char *src, int n);
+
+char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+char *_memset(char *s, char b, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_strchr(char *s, char c);
+
+unsigned int _strspn(char *s, char *accept);
+
+char *_strpbrk(char *s, char *accept);
+
+char *_strstr(char *haystack, char *needle);
+
+#endif /* MAIN_H */
